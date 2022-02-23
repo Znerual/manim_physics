e45049a39260aa37e5538159b2c270ebccc2a7b2
Laurenz Ruzicka <laurenz_ruzicka@gmx.at>
2022-02-23 23:05:28 +0100

    working instance, changed scaling to work in 2D

--- src/manim_physics/objects/spring2D.py
+++ src/manim_physics/objects/spring2D.py
@@ -1,15 +1,27 @@
 from dataclasses import dataclass, field
 
+import numpy as np
+from manim import *
+from numpy.typing import NDArray
+
+from typing import Union
+from math import acos, cos, sin
+
+from manim_physics.objects.mass import Mass
+from manim_physics.objects.wall import Wall
+from manim_physics.utils.id_generator import Id_generator
 
 @dataclass
-class Spring(Line):
+class Spring2D(Line):
     """
     Represents a spring like object that needs two masses to attach to (can also be a wall).
     Setting the spring_constant behavior changes the forces created.
     Force = k * (x_rest - x), which is added to the force variable of the attached masses.
     """
-    id_hash: int = field(init=False, default_factory=id_generator.get_new_id)
+    id_hash: int = field(init=False, default_factory=Id_generator.get_new_id)
     rest_width: float = field(init=False, default=None)
+    alpha_change: float = field(init=False, default=0)
+    current_stretch : float = field(init=False, default=1)
     spring_constant: float
     start_mass: Union[Mass, Wall]
     end_mass: Union[Mass, Wall]
@@ -20,7 +32,7 @@ class Spring(Line):
     fill_opacity: float = field(default=1)
     stroke_width: float = field(default=3)
     fill_color: str = field(default=None)
-    sheen_direciton: typing.NDArray[float] = field(default=UP)
+    sheen_direction: NDArray[float] = field(default=UP)
     sheen_factor: float = field(default=0.5)
 
     def __post_init__(self):
@@ -32,35 +44,39 @@ class Spring(Line):
             the right place. It also calculates the creates forces and adds them to the attached masses force variable
             """
 
-            # scaling depends on the starting and end point of the spring
-            r1, r2 = mobj.get_end_points()
-
             # get position vectors
-            end_mass_pos = self.end_mass.get_center()
-            start_mass_pos = self.start_mass.get_center()
+            end_mass_pos = mobj.end_mass.get_center()
+            start_mass_pos = mobj.start_mass.get_center()
+
+            #print(f"mobj end mass: {mobj.end_mass} pos: {end_mass_pos}")
 
             difference_mass_vec = (end_mass_pos - start_mass_pos)
-            unit_spring_vec = difference_mass_vec / np.linalg.norm(difference_mass_vec)
-            difference_spring_vec = r2 - r1
+            unit_mass_vec = difference_mass_vec / np.linalg.norm(difference_mass_vec, 2)
 
-            # avoid division by 0 and stretch geometry
-            if abs(difference_spring_vec[0]) > 0.0000001:
-                mobj.stretch(difference_mass_vec[0] / difference_spring_vec[0], dim=0)
-                mobj.label.stretch(difference_spring_vec[0] / difference_mass_vec[0],
-                                   dim=0)  # invert stretching on label
-            if abs(difference_spring_vec[1]) > 0.0000001:
-                mobj.stretch(difference_mass_vec[1] / difference_spring_vec[1], dim=1)
-                mobj.label.stretch(difference_spring_vec[1] / difference_mass_vec[1],
-                                   dim=1)  # invert stretching on label
+            stretch_factor = np.linalg.norm(difference_mass_vec, 2) / self.rest_width
+            delta_stretch = stretch_factor - self.current_stretch
+            self.current_stretch = stretch_factor
+            
+            # get rotation
+            alpha = acos(np.dot(RIGHT, unit_mass_vec)) if unit_mass_vec[1] > 0 else -acos(np.dot(RIGHT, unit_mass_vec))
+            delta_alpha = (alpha - mobj.alpha_change)  # * 0.5
+            mobj.alpha_change = alpha
+            if delta_alpha != 0:
+                # print(f"delta alpha: {delta_alpha}, unit mass: {unit_mass_vec}, unit spring {unit_spring_vec}, r1 {r1}, r2 {r2}")
+                mobj.rotate(delta_alpha)
 
+            mobj.stretch(delta_stretch * cos(alpha) + 1, dim=0)
+            mobj.stretch(delta_stretch * sin(alpha) + 1, dim=1)
+            
             # calculate old vs new positions centers and move spring accordingly
-            mobj.shift(((end_mass_pos + start_mass_pos) * 0.5 - (r1 + r2) * 0.5))
+            mobj.shift(((end_mass_pos + start_mass_pos) * 0.5 - mobj.get_center()))
 
             # calculate forces
-            self.start_mass.force += self.spring_constant * (
-                    self.rest_width - np.linalg.norm(difference_mass_vec)) * -unit_spring_vec
-            self.end_mass.force += self.spring_constant * (
-                    self.rest_width - np.linalg.norm(difference_mass_vec)) * unit_spring_vec
+            
+            mobj.start_mass.force += mobj.spring_constant * (
+                    mobj.rest_width - np.linalg.norm(difference_mass_vec)) * -unit_mass_vec
+            mobj.end_mass.force += mobj.spring_constant * (
+                    mobj.rest_width - np.linalg.norm(difference_mass_vec)) * unit_mass_vec
 
         # create spring
 
@@ -70,12 +86,12 @@ class Spring(Line):
 
         # unit vector for the direction of the spring
         direction_vec = (end_mass_center - start_mass_center)
-        direction_vec /= np.linalg.norm(direction_vec)
+        direction_vec /= np.linalg.norm(direction_vec, 2)
 
         # unit normal vector orthogonal to direction vector
         normal_vec = np.array([direction_vec[1], - direction_vec[0], 0])
 
-        section_length = (np.linalg.norm(end_mass_center - start_mass_center) / (
+        section_length = (np.linalg.norm(end_mass_center - start_mass_center, 2) / (
                 self.length + 1)) * 0.5
 
         # init wiggly line directions
@@ -85,16 +101,16 @@ class Spring(Line):
         halve_height = 1 - np.absolute(normal_vec) * 0.5  # for starting and ending orthogonal
 
         # calculate and save rest_width for force calculation
-        self.rest_width = np.linalg.norm(end_mass_center - start_mass_center)
+        self.rest_width = np.linalg.norm(end_mass_center - start_mass_center, 2)
 
         # draw spring lines
         # first horizontal line
         cur_start = start_mass_center
         cur_end = start_mass_center + horizontal
-        super(Spring, self).__init__(start=cur_start, end=cur_end, color=self.fill_color,
-                                     fill_opacity=self.fill_opacity,
-                                     stroke_width=self.stroke_width, sheen_direction=self.sheen_direciton,
-                                     sheen_factor=self.sheen_factor)
+        super(Spring2D, self).__init__(start=cur_start, end=cur_end, color=self.fill_color,
+                                       fill_opacity=self.fill_opacity,
+                                       stroke_width=self.stroke_width, sheen_direction=self.sheen_direction,
+                                       sheen_factor=self.sheen_factor)
 
         # wiggly up down lines
         for j in range(2 * self.length):
@@ -111,17 +127,24 @@ class Spring(Line):
                     cur_end += down_right
 
             self.add(Line(cur_start, cur_end, color=self.fill_color, fill_opacity=self.fill_opacity,
-                          stroke_width=self.stroke_width, sheen_direction=self.sheen_direciton,
+                          stroke_width=self.stroke_width, sheen_direction=self.sheen_direction,
                           sheen_factor=self.sheen_factor))
 
         # last horizontal line
         self.end_line = Line(cur_end, cur_end + horizontal, color=self.fill_color, fill_opacity=self.fill_opacity,
-                             stroke_width=self.stroke_width, sheen_direction=self.sheen_direciton,
+                             stroke_width=self.stroke_width, sheen_direction=self.sheen_direction,
                              sheen_factor=self.sheen_factor)
+
         self.add(self.end_line)
 
-        self.label = self.get_label()
-        self.add(self.label)
+        # add label
+        if self.label_text:
+            self.label = self.get_label()
+            self.add(self.label)
+
+        self.alpha_change = acos(np.dot(RIGHT, direction_vec))
+
+        # add dynamic stretching
         self.add_updater(update_stretch)
 
     def __hash__(self):
@@ -136,8 +159,12 @@ class Spring(Line):
         """ Returns the start and end point of the spring"""
         return self.points[0], self.end_line.points[-1]
 
+    def spring_constant_to_label_text(self):
+        self.label_text = "{:,}\\,N/m".format(round(self.spring_constant))
+
     def get_label(self):
         """ Creates and places a label"""
+
         label = Tex(self.label_text)
         label.scale(self.label_scale_value)
         label.next_to(self, UP, SMALL_BUFF)
