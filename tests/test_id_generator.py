e37e9c789b38c8aa16f68c2f20c021428dd3b085
Laurenz Ruzicka <laurenz_ruzicka@gmx.at>
2022-02-23 23:05:41 +0100

    Added simple tests

--- tests/test_id_generator.py
+++ tests/test_id_generator.py
@@ -1,4 +1,24 @@
-from manim_physics.id_generator import Id_generator
+import pytest
 
-def test_generation():
-    pass
+from manim_physics.utils.id_generator import Id_generator
+from manim_physics.objects.mass import Mass
+from manim_physics.objects.wall import Wall
+from manim_physics.objects.spring2D import Spring2D
+
+
+def test_generation1(reset_id_generator):
+    """ Create multiple objects and check id generation"""
+    mass1 = Mass(12)
+    assert mass1.id_hash == 1
+    mass2 = Mass(12)
+    assert mass2.id_hash == 2
+    mass3 = Mass(13)
+    assert mass3.id_hash == 3
+
+def test_generation2(reset_id_generator):
+    """ Create multiple objects and check id generation"""
+    mass1 = Mass(12)
+    mass2 = Mass(12)
+    assert mass2.id_hash == 2
+    mass3 = Mass(13)
+
---
+++ tests/test_mass.py
@@ -0,0 +1,4 @@
+from manim_physics.objects.mass import Mass
+
+def test_mass():
+    mass = Mass(2)
